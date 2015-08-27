c12cb8c51af916bcd85132757e4130e384510a60
Ben Anderman <bena@evidentpoint.com>
2015-08-26 21:46:06 -0700

    Added some layout

--- academia/app/controllers/application_controller.rb
+++ academia/app/controllers/application_controller.rb
@@ -2,4 +2,34 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by_authentication_token(cookies[:auth_token]) if (cookies[:auth_token])
+
+    @current_user
+  end
+  
+  protected
+
+  def authenticate_user
+    session[:user_id].present?()
+  end
+
+  def validate_logged_in
+    forbidden() unless current_user
+  end
+
+  def validate_write_permissions
+    unauthorized() unless has_permission?('write')
+  end
+
+  def validate_read_permissions
+    unauthorized() unless has_permission?('read')
+  end
+
+  def save_login_state
+    !session[:user_id].present?()
+  end
+  
 end
--- academia/app/models/user.rb
+++ academia/app/models/user.rb
@@ -1,3 +1,39 @@
 class User < ActiveRecord::Base
-  has_secure_password
+  
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    length: { in: 3..50 },
+    format: /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
+  validates :name,
+    presence: true
+
+  before_save :encrypt_password
+  after_save :clear_password
+
+  def encrypt_password
+    if (self.password_changed?())
+      self.password_salt = BCrypt::Engine.generate_salt()
+      self.password = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+
+  def clear_password
+    self.password = nil
+  end
+
+  def self.authenticate(email = "", login_password = "")
+    user = User.find_by_email(email)
+
+    if (user && user.match_password(login_password))
+      return user
+    else
+      return false
+    end
+  end
+
+  def match_password(login_password = "")
+    password == BCrypt::Engine.hash_secret(login_password, password_salt)
+  end
+
 end
