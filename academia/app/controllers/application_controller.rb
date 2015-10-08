72480e3025bb867780bd659855974c165be85ad6
Ben Anderman <bena@evidentpoint.com>
2015-10-07 20:35:19 -0700

    Add login functionality

--- academia/app/controllers/application_controller.rb
+++ academia/app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  include SessionsHelper
+
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     @current_user ||= User.find_by_authentication_token(cookies[:auth_token]) if (cookies[:auth_token])
--- academia/config/routes.rb
+++ academia/config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root to: 'home#index'
 
   get 'home/index'
   get 'signup' => 'users#new'
   resources :users
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
